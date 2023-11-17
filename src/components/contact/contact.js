8f9ef2088270c4b49741ab62cd3cdfb07599be14
maria alvarez <alvarezzeaemaria@gmail.com>
2023-11-16 23:28:51 -0600

    refactor scroll

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,18 +1,12 @@
 import { Component } from "react";
-
+import "../contact/contact.scss";
 class Contact extends Component{
-
-
-    render(){
-        
-        return (<div>
-             <h1>CONTACT SECTION </h1>
-           
-        </div>);
-           
-           
-       
+    render(){   
+        return (
+        <div
+            className="contactSection" id="section3">
+             <h1>CONTACT SECTION </h1> 
+        </div>);   
     }
-}
-  
+}  
   export default Contact;
--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -6,12 +6,12 @@ class Navigation extends Component {
 
     this.scrollToSection = this.scrollToSection.bind(this);
   }
-  
+
   scrollToSection(sectionId) {
     const section = document.querySelector(sectionId);
     if (section) {
       window.scrollTo({
-        behavior: 'smooth',
+        behavior: 'instant',
         top: section.offsetTop,
       });
       this.setState({ activeLink: sectionId });
@@ -24,28 +24,15 @@ class Navigation extends Component {
     return (
     <nav className="navigation">
       <ul>
-        <li><a href="/">home</a></li>
-        <li><a href="#" onClick={() => this.scrollToSection('#section1')}>about</a></li>
-        <li><a href="#" onClick={() => this.scrollToSection('#section2')}>experience</a></li>
-        <li><a href="/contact">contact</a></li>
+        <li>Logo</li>
+        <li><button onClick={() => this.scrollToSection('#section1')}>about</button></li>
+        <li><button onClick={() => this.scrollToSection('#section2')}>experience</button></li>
+        <li><button onClick={() => this.scrollToSection('#section3')}>contact</button></li>
       </ul>
   </nav>
 
     );
   }
-//   handleClick() {
-//     this.setState({property: 'clicked'});
-//  }
+
 }
 export default Navigation; 
-
-
-// Example of Functional Component 
-  // function NavBar(){
-  //   return (
-  //     <div className="navBar">
-  //       <p className="text-3xl font-bold underline"> Top</p>
-  //     </div>
-  //   );
-  // }
-  // export default NavBar; 
