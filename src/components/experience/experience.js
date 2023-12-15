97f7c07002a19fe0b21f3e350518e0851f7a9ee8
maria alvarez <alvarezzeaemaria@gmail.com>
2023-12-15 14:38:46 -0600

    edits to section

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -3,7 +3,16 @@ import React from 'react';
 
 function Experience() {
   return (
-    <div className="experienceSection" id="section2">
+    <div className="experience-section" id="section2">
+      <div className="experience-wrapper">
+        <h1>What I've Done</h1>
+        <p>This component is currently undergoing reconstruction. <br></br> However, below is a quick glimpse of the amazing things I've built. </p>
+        <button className="button" id="button-2">
+            <div id="slide"></div>
+            <a href={require("../../assets/resume.pdf")} download="resumeFile">View Resume</a>         
+      </button>
+
+      </div>
     </div>
   );
 }
