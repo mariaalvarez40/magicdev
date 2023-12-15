fb9466860b9652e44d2356211a0f3638ea344e81
maria alvarez <alvarezzeaemaria@gmail.com>
2023-12-15 15:21:50 -0600

    add content

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -3,8 +3,14 @@ import React from 'react';
 
 function Contact() {
   return (
-    <div className="contactSection" id="section3">
-    </div>
+    <div className="contact-section" id="section3">
+    <div className="contact-wrapper">
+      <div className="contact-content">
+        <h1>Let's collaborate on your next project</h1>
+      </div>
+      <div className="contact-image"></div>
+    </div>   
+  </div>
   );
 }
 
