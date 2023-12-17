c4e65175196aadd1194d1b50255c281a93111cd3
maria alvarez <alvarezzeaemaria@gmail.com>
2023-12-17 15:49:43 -0600

    create contact content

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -7,6 +7,14 @@ function Contact() {
     <div className="contact-wrapper">
       <div className="contact-content">
         <h1>Let's collaborate on your next project</h1>
+        <div className="contact-socials">
+            <a href="mailto:alvarezzeamaria@gmail.com">
+              <div className="gmail"></div>
+            </a>
+            <a href="https://www.linkedin.com/in/alvarezmaria1/" rel="noopener noreferrer" target="_blank">
+              <div className="linkedIn"></div>
+            </a> 
+        </div>
       </div>
       <div className="contact-image"></div>
     </div>   
